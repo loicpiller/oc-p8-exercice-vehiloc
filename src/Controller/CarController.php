3fa14b179dce6aa711cb7b74815005eb4a9c448d
Loïc PILLER <loic.piller@etu.univ-nantes.fr>
2025-05-09 16:35:28 +0200

    handle car deletion

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -6,15 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\CarRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class CarController extends AbstractController
 {
     #[Route('/car/{id}', name: 'car_details', requirements: ['id' => '\d+'])]
-    public function index(int $id, CarRepository $carRepository): Response
+    public function details(int $id, CarRepository $carRepository): Response
     {
         $data['car'] = $carRepository->find($id);
-        $data['car'] ?? throw $this->createNotFoundException("Car not find.");
+        $data['car'] ?? throw $this->createNotFoundException("Car not found.");
 
         return $this->render('car/details.html.twig', $data);
+    }
+
+    #[Route('car/{id}/delete', name: 'car_delete', requirements: ['id' => '\d+'])]
+    public function delete(int $id, CarRepository $carRepository, EntityManagerInterface $entityManager): Response
+    {
+        $car = $carRepository->find($id);
+        $car ?? throw $this->createNotFoundException("Car not found.");
+
+        $entityManager->remove($car);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_home');
     }
 }
